431b3260e175c3626f05a9eb358cceda795f829f
RACE (Rasmus Cederdorff) <rasmus@cederdorff.com>
2024-04-05 10:18:58 +0200

    Update email prop to mail prop in User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,8 @@
-export default function User({ name, email, title }) {
+export default function User({ name, mail, title }) {
   return (
     <div>
       <h2>{name}</h2>
-      <p>Email: {email}</p>
+      <p>Mail: {mail}</p>
       <p>Title: {title}</p>
     </div>
   );
